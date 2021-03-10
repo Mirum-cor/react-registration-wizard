9659ad153f35dc059d76eb664da0f585a60a6e16
Mirum-cor <laetitia-caritas@mail.ru>
2021-03-11 00:11:42 +0300

    feat: add sidebar component and first screen of form-filling component

--- src/components/FormFilling.js
+++ src/components/FormFilling.js
@@ -5,7 +5,63 @@ export default class FormFilling extends Component {
   state = {};
   render() {
     return (
-      <div className="form-filling"></div>
+      <div className="form-filling">
+        <div className="first-screen">
+          <form>
+            <div className="field-form">
+              <label htmlFor="salutation" className="required-field-sign">salutation:</label>
+              <select id="salutation">
+                <option></option>
+                <option>1</option>
+                <option>2</option>
+              </select>
+            </div>
+            <div className="field-form">
+              <label htmlFor="first-name" className="required-field-sign">first name:</label>
+              <input id="first-name" type="text"></input>
+            </div>
+            <div className="field-form">
+              <label htmlFor="middle-name">middle name:</label>
+              <input id="middle-name" type="text"></input>
+            </div>
+            <div className="field-form">
+              <label htmlFor="last-name" className="required-field-sign">last name:</label>
+              <input id="last-name" type="text"></input>
+            </div>
+            <div className="field-form">
+              <label htmlFor="company" className="required-field-sign">company:</label>
+              <input id="company" type="text"></input>
+            </div>
+            <div className="field-form">
+              <label htmlFor="title" className="required-field-sign">title:</label>
+              <input id="title" type="text"></input>
+            </div>
+          </form>
+          <form>
+            <div className="field-form">
+              <label htmlFor="email" className="required-field-sign">email:</label>
+              <input id="email" type="email"></input>
+              <p className="note">email wiil be your login</p>
+            </div>
+            <div className="field-form">
+              <label htmlFor="confirm-email" className="required-field-sign">confirm email:</label>
+              <input id="confirm-email" type="email"></input>
+            </div>
+            <div className="field-form">
+              <label htmlFor="phone" className="required-field-sign">phone:</label>
+              <input id="phone" type="tel" placeholder="(   )   -"></input>
+            </div>
+            <div className="field-form">
+              <label htmlFor="fax">fax:</label>
+              <input id="fax" type="tel" placeholder="(   )   -"></input>
+            </div>
+            <div className="field-form">
+              <label htmlFor="mobile">mobile:</label>
+              <input id="mobile" type="tel" placeholder="(   )   -"></input>
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,32 @@ export default class Sidebar extends Component {
   state = {};
   render() {
     return (
-      <div className="sidebar"></div>
-    )
+      <div className='sidebar'>
+        <div className='sidebar-wrapper'>
+          <ul>
+            <li className='current-step'>
+              <span className='next-steps'>&nbsp;</span>
+              <span className='next-steps-name'>Contact Info</span>
+            </li>
+            <li>
+              <span className='next-steps'>2</span>
+              <span className='next-steps-name'>Areas</span>
+            </li>
+            <li>
+              <span className='next-steps'>3</span>
+              <span className='next-steps-name'>Address</span>
+            </li>
+            <li>
+              <span className='next-steps'>4</span>
+              <span className='next-steps-name'>Password</span>
+            </li>
+            <li>
+              <span className='next-steps'>5</span>
+              <span className='next-steps-name'>Completed</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
